3612fe120fe6bd4e6652c011c2fcc42b32c4282c
John_Titor <825877689@qq.com>
2017-03-22 21:04:51 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/jp/co/daifuku/coolweather/android/db/Province.java
@@ -0,0 +1,36 @@
+package jp.co.daifuku.coolweather.android.db;
+
+import org.litepal.crud.DataSupport;
+
+public class Province extends DataSupport {
+
+    private int id;
+
+    private int provinceCode;
+
+    private String provinceName;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+}
